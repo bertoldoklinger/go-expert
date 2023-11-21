e8c3beaef458a604d0561374c0e027557657bcf8
Bertoldo Klinger <bertoldokbrj@gmail.com>
2023-11-20 22:22:08 -0300

    Add dependencies and update code

--- 18/main.go
+++ 18/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"curso-go/matematica"
+	"fmt"
+
+	"github.com/google/uuid"
 )
 
 func main() {
 
-	s := matematica.Soma(10,20)
-	
+	s := matematica.Soma(10, 20)
+	carro := matematica.Carro{Marca: "Fiat"}
 	fmt.Printf("Resultado: %v", s)
+	fmt.Println(matematica.A)
+	fmt.Println(carro.Marca)
+	matematica.Andar(&carro)
+	fmt.Println(uuid.New())
 }
